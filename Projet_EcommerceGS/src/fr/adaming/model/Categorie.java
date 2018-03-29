5613620b328e4e3a465d82481c6ce9b598c7fe0c
inti0433 <inti0433@DESKTOP-2IUV67P>
2018-03-29 13:27:43 +0200

    shams:class categorie et client

---
+++ Projet_EcommerceGS/src/fr/adaming/model/Categorie.java
@@ -0,0 +1,73 @@
+package fr.adaming.model;
+
+import java.util.Arrays;
+
+public class Categorie {
+
+	// attributs
+	private Long idCategorie;
+	private String nomCategorie;
+	private byte photo[];
+	private String description;
+
+	// constructeur
+	public Categorie() {
+		super();
+	}
+
+	public Categorie(String nomCategorie, byte[] photo, String description) {
+		super();
+		this.nomCategorie = nomCategorie;
+		this.photo = photo;
+		this.description = description;
+	}
+
+	public Categorie(Long idCategorie, String nomCategorie, byte[] photo, String description) {
+		super();
+		this.idCategorie = idCategorie;
+		this.nomCategorie = nomCategorie;
+		this.photo = photo;
+		this.description = description;
+	}
+
+	// getters et setters
+	public Long getIdCategorie() {
+		return idCategorie;
+	}
+
+	public void setIdCategorie(Long idCategorie) {
+		this.idCategorie = idCategorie;
+	}
+
+	public String getNomCategorie() {
+		return nomCategorie;
+	}
+
+	public void setNomCategorie(String nomCategorie) {
+		this.nomCategorie = nomCategorie;
+	}
+
+	public byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Categorie [idCategorie=" + idCategorie + ", nomCategorie=" + nomCategorie + ", photo="
+				+ Arrays.toString(photo) + ", description=" + description + "]";
+	}
+
+}
---
+++ Projet_EcommerceGS/src/fr/adaming/model/Client.java
@@ -0,0 +1,81 @@
+package fr.adaming.model;
+
+public class Client {
+	// declartion des attributs
+	private Long idClient;
+	private String nomClient;
+	private String adresse;
+	private String email;
+	private String tel;
+
+	// contructeur(vide,sans id et avec id)
+	public Client() {
+		super();
+	}
+
+	public Client(String nomClient, String adresse, String email, String tel) {
+		super();
+		this.nomClient = nomClient;
+		this.adresse = adresse;
+		this.email = email;
+		this.tel = tel;
+	}
+
+	public Client(Long idClient, String nomClient, String adresse, String email, String tel) {
+		super();
+		this.idClient = idClient;
+		this.nomClient = nomClient;
+		this.adresse = adresse;
+		this.email = email;
+		this.tel = tel;
+	}
+
+	// getters et setters
+	public Long getIdClient() {
+		return idClient;
+	}
+
+	public void setIdClient(Long idClient) {
+		this.idClient = idClient;
+	}
+
+	public String getNomClient() {
+		return nomClient;
+	}
+
+	public void setNomClient(String nomClient) {
+		this.nomClient = nomClient;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	// @toString
+	@Override
+	public String toString() {
+		return "Client [idClient=" + idClient + ", nomClient=" + nomClient + ", adresse=" + adresse + ", email=" + email
+				+ ", tel=" + tel + "]";
+	}
+
+}
